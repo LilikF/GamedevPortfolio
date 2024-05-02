c867998fdcaa20568b6c80698b039686eaabd466
Lilik Ferenc <ferenc.lilik.development@gmail.com>
2024-05-03 01:42:50 +0200

    fix: closing

--- portfolio/src/Components/Sections/Closing/Closing.jsx
+++ portfolio/src/Components/Sections/Closing/Closing.jsx
@@ -8,7 +8,7 @@ const Closing=()=>{
             <>
             <div>{`Since, as I understand, this job focuses on tool development for game dev, I focused exclusively on a few in-editor tools I made for Godot.`}</div>
             <br></br>
-            <div>{`However, as you can hopefully see from the provided material, I am confident in my ability and knowledge regarding game engines. Since I can't prove my technical skills in a simple portfolio, I sincerely hope you give me a chance in the form of an interview. As for my motivation, believe me, it is sky high when it comes to this sort of job, and I am looking forward to hearing from you.`}</div>
+            <div>{`However, as you can hopefully see from the provided material, I am confident in my ability and knowledge regarding game development practices. Since I can't prove my technical skills in a simple portfolio, I sincerely hope you give me a chance in the form of an interview. As for my motivation, believe me, it is sky high when it comes to this sort of job, and I am looking forward to hearing from you.`}</div>
             </>
         }>
         </TextCard>
