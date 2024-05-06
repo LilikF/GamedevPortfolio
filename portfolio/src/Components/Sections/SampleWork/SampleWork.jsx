6c4090d6092d96c5905f8bb10ac65f6d8bf0cc64
Lilik Ferenc <ferenc.lilik.development@gmail.com>
2024-05-06 10:04:08 +0200

    fix: allow iframe fullscreen

--- portfolio/src/Components/Sections/SampleWork/SampleWork.jsx
+++ portfolio/src/Components/Sections/SampleWork/SampleWork.jsx
@@ -38,7 +38,7 @@ const SampleWork=()=>{
                 </div>
             </>
         }>
-            <iframe width="100%" height="315" src="https://www.youtube.com/embed/1h8LbbCYkss?si=SFvkEKc5Mgtl3sHy" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+            <iframe width="100%" height="315" src="https://www.youtube.com/embed/1h8LbbCYkss?si=SFvkEKc5Mgtl3sHy" title="YouTube video player" frameborder="0" allow="accelerometer; fullscreen; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
         </TextCard>
     </FullWidthGrid>
     </>
